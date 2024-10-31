f73e98c3bd1b37a11e93b3af3f341ecd88fb44ab
Fred <fred.carvalho10@hotmail.com>
2024-10-31 20:59:53 -0300

    Início projeto

---
+++ script.js
@@ -0,0 +1,3 @@
+function learnMore() {
+    alert("Bem-vindo à nossa plataforma de suporte sustentável! Navegue para saber mais sobre como otimizar o uso de água e melhorar a sua produtividade.");
+}
